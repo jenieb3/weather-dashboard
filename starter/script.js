d7250a5bc38f2b2175734edef1ad727c2736195b
jenelle Spio-Garbrah <jenelle.garbrah3@gmail.com>
2023-01-27 22:17:24 +0000

    added click event to get data

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,18 @@
-console.log('hello')
+//console.log('hello')
+var APIkey = '379b0ba619900b8104142245c7037879';
+var submitBtn = document.getElementById('search-button');
+var searchInput = document.getElementById('search-input');
+
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var city = searchInput.value;
+    var currWeatherAPI = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIkey;
+
+    $.ajax({
+        url: currWeatherAPI,
+        method: 'GET'
+    }).then(function(response) {
+        console.log(response);
+        // code to display the current weather forecast for the provided city
+    });
+});
